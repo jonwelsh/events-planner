6514399ceba6b2f8a88eb67964c40057c77044de
Jon <27546202+jonwelsh@users.noreply.github.com>
2025-06-03 21:26:27 +0100

    feat: add attending data fetching to home route and update SeedPreview component

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { getSeedPreview, getUsers } from '~/lib/api'
+import { getSeedPreview, getUsers, getAttending } from '~/lib/api'
 import { SeedPreview } from '~/components/SeedPreview'
 import { useSuspenseQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/')({
+  // This loader function will run on the server when the route is accessed
+  // and will ensure that the data is available before rendering the component.
   loader: async ({ context }) => {
     await context.queryClient.ensureQueryData({
       queryKey: ['events'],
@@ -13,11 +15,16 @@ export const Route = createFileRoute('/')({
       queryKey: ['users'],
       queryFn: getUsers
     })
+    await context.queryClient.ensureQueryData({
+      queryKey: ['attending'],
+      queryFn: getAttending
+    })
   },
   component: Home
 })
 
 function Home() {
+  // Use the useSuspenseQuery hook to fetch data
   const { data: events } = useSuspenseQuery({
     queryKey: ['events'],
     queryFn: getSeedPreview
@@ -26,10 +33,16 @@ function Home() {
     queryKey: ['users'],
     queryFn: getUsers
   })
+  const { data: attending } = useSuspenseQuery({
+    queryKey: ['attending'],
+    queryFn: getAttending
+  })
+
+  console.log(attending)
   return (
     <div className='p-8 space-y-8'>
       <h1 className='font-bold '>Welcome home!</h1>
-      <SeedPreview events={events} users={users} />
+      <SeedPreview events={events} users={users} attending={attending} />
     </div>
   )
 }
